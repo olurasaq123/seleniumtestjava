33cb5845755339586476f16d379bf10680b028a1
Olu Rasaq <olu.rasaq@ceracare.co.uk>
2024-09-25 23:33:16 +0100

    updated

--- sampleTestFramework/src/test/java/runners/RunCukeTest.java
+++ sampleTestFramework/src/test/java/runners/RunCukeTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        //features = "classpath:features"
-        features = "src//test//resources//features"
+        features = "classpath:features"
+        //features = "src\\test\\resources\\features"
         ,glue={"stepdefinitions"},
         plugin ={"html:target/cucumber-report/cucumber.html"}
 )
